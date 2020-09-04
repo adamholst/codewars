6e0948b54b967caba92ef98ee69b7ff1db3dbd47
adamholst <69865214+adamholst@users.noreply.github.com>
2020-09-04 18:49:21 -0500

    Create 7kyu_16+18=214.py

---
+++ 7kyu_16+18=214.py
@@ -0,0 +1,27 @@
+'''
+For this Kata you will have to forget how to add two numbers together.
+
+In simple terms, our method does not like the principle of carrying over
+numbers and just writes down every number it calculates :-)
+'''
+
+def add(num1, num2):
+  a = []
+  count = 1
+  num1 = str(num1)
+  num2 = str(num2)
+  if len(num1) < len(num2):
+    num1 = num1.zfill(len(num2))
+  if len(num1) > len(num2):
+    num2 = num2.zfill(len(num1))
+  for j in num2:
+    b = int(num1[len(num1)-count]) + int(num2[len(num2)-count])
+    d = str(b)
+    count += 1
+    a.insert(0,d)
+  return int("".join(a))
+
+def onliner(num1, num2):
+  return int(str(add(a//10,b//10))+str(a%10+b%10)) if a*b else a+b
+
+print(add(16, 18))
